21120d8dcfbc3a9170e6b4a458818e194c3fc8b1
ixejinn <174031282+ixejinn@users.noreply.github.com>
2024-10-07 03:02:03 +0900

    test: Texture

--- October/State/Sample.cpp
+++ October/State/Sample.cpp
@@ -8,6 +8,8 @@
 #include "../GameObject/GameObject.h"
 #include "../Component/FixedUpdatable/Transform.h"
 
+#include "../Graphics/stb_image.h"
+
 namespace Manager
 {
     extern GameObjectManager& objMgr;
@@ -57,14 +59,15 @@ void Sample::Init()
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f  // top left 
     };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // upper triangle
-        1, 2, 3   // lower triangle
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     glGenVertexArrays(1, &VAO); // VAO stores the state related to vertex attribute settings
@@ -81,25 +84,63 @@ void Sample::Init()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // and then configure vertex attributes(s)
-    // (Connect the data of the VBO - which is bound to GL_ARRAY_BUFFER - to the vertex attribute array.
-    // The VBO no longer needs to be bound to GL_ARRAY_BUFFER
-    // The vertex data information of the VBO is stored in the VAO.)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    
-    // Even if the binding of GL_ARRAY_BUFFER is released,
-    // the necessary data for the vertex attributes is still accessible
-    // (due to the glVertexAttribPointer call)
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //// and then configure vertex attributes(s)
+    //// (Connect the data of the VBO - which is bound to GL_ARRAY_BUFFER - to the vertex attribute array.
+    //// The VBO no longer needs to be bound to GL_ARRAY_BUFFER
+    //// The vertex data information of the VBO is stored in the VAO.)
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    //glEnableVertexAttribArray(0);
+    //
+    //// Even if the binding of GL_ARRAY_BUFFER is released,
+    //// the necessary data for the vertex attributes is still accessible
+    //// (due to the glVertexAttribPointer call)
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // Releasing the binding of the VAO means that the state of this VAO can no longer be modified.
-    // Even after unbinding, the vertex attributes and the data of the VBO remain intact.
-    glBindVertexArray(0);
+    //// Releasing the binding of the VAO means that the state of this VAO can no longer be modified.
+    //// Even after unbinding, the vertex attributes and the data of the VBO remain intact.
+    //glBindVertexArray(0);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    unsigned char* data = stbi_load("Assets/PlanetTexture.png", &width, &height, &nrChannels, STBI_rgb_alpha);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void Sample::Update()
 {
+    glBindTexture(GL_TEXTURE_2D, texture);
+
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -110,5 +151,5 @@ void Sample::Exit()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
+    //glDeleteProgram(shaderProgram);
 }
